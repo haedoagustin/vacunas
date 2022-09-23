30a3a50f3bc8c771351dfbdf1832e3ba71b97243
Agustin Haedo <haedoagustin@gmail.com>
2022-09-22 21:40:36 -0300

    Confirm page style

--- pages/confirm.vue
+++ pages/confirm.vue
@@ -5,7 +5,7 @@ definePageMeta({
 })
 const user = useSupabaseUser()
 
-watchEffect(() => {
+watch(user, () => {
     if (user.value) {
         navigateTo('/')
     }
@@ -13,8 +13,8 @@ watchEffect(() => {
 </script>
     
 <template>
-    <NuxtLayout name="blank">
-        <div class="block">
+    <NuxtLayout>
+        <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
             Ingresando...
         </div>
     </NuxtLayout>
