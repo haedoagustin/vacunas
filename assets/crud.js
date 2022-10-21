e5ad8fc94ab86372906cf8e5c6552f32cd586411
Agustin Haedo <haedoagustin@gmail.com>
2022-10-20 23:13:42 -0300

    Error si no hay dosis

--- assets/crud.js
+++ assets/crud.js
@@ -128,12 +128,12 @@ export const getEnviosParaVacunacion = async (
 ) => {
   let { data, error } = await client
     .from("envios")
-    .select("*, lote_id!inner(vacuna_desarrollada_id!inner(*))")
+    .select("*, lote_id!inner(id, vacuna_desarrollada_id!inner(*))")
     .eq("lote_id.vacuna_desarrollada_id.id", vacuna_desarrollada_id)
     .eq("jurisdiccion_id", jurisdiccion_id)
     .gt("cantidad_disponible", 0);
 
-  if (!data) {
+  if (!data.length) {
     error =
       "No hay dosis disponibles. La vacuna seleccionada no tiene más dosis disponibles en la jurisdicción en la que usted se encuentra vacunando. Compruebe que haya seleccionado la vacuna y el laboratorio correctos.";
   }
