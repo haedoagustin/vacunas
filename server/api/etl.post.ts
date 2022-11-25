a60a0fe43be35fd4cee51ddcbf2010d1fa0ce824
Agustin Haedo <haedoagustin@gmail.com>
2022-11-25 18:32:57 -0300

    ETL endpoint bootstrap

---
+++ server/api/etl.post.ts
@@ -0,0 +1,17 @@
+import { serverSupabaseClient, serverSupabaseUser } from "#supabase/server";
+
+export default eventHandler(async (event) => {
+    
+    // ETL de d_lugar
+    
+    // ETL de d_tiempo
+    
+    // ETL de d_vacuna
+
+    // ETL de d_vacunado
+
+    // ETL de h_vacunados
+
+    // ETL de h_vencidas
+
+});
