4cddaaae337c962a81b54f3551f71f1a42b93ce5
Agustin Haedo <haedoagustin@gmail.com>
2022-12-14 13:46:05 -0300

    Vacunación se crea con trigger en base de datos

--- server/api/vacunacion.post.ts
+++ server/api/vacunacion.post.ts
@@ -21,25 +21,9 @@ export default defineEventHandler(async (event) => {
   const { data, error } = await client
     .from("vacunaciones")
     .insert(vacunacion)
-    .select("*, envio_id(id, cantidad_disponible)")
     .single();
 
   if (error) throw `Algo salio mal registrando una vacunación: ${error.message}`;
-
-  try {
-    // Decrementar el stock de vacunas
-    await client
-      .from("envios")
-      .update({
-        cantidad_disponible: --data.envio_id.cantidad_disponible,
-      })
-      .eq("id", data.envio_id.id);
-  } catch (err) {
-    // Elimina la vacunación recién creada por error al decrementar la
-    // cantidad del envio
-    await client.from("vacunaciones").delete().eq("id", data.id);
-
-    throw err;
-  }
+  
   return data;
 });
